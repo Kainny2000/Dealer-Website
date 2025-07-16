d8b155fe6a9569c3557593f9060364ce268cdaf2
Kainny Godinez <kainny2000@gmail.com>
2025-07-16 08:10:04 -0600

    start astro project template

---
+++ src/data/vehicles.ts
@@ -0,0 +1,20 @@
+export const vehicles = [
+    {
+        make: "Toyota",
+        model: "Camry",
+        condition: "new",
+        year: 2024,
+        price: 35000,
+        vin: "123456789",
+        image: "/placeholder.svg",
+    },
+    {
+        make: "Honda",
+        model: "Civic",
+        condition: "used",
+        year: 2022,
+        price: 25000,
+        vin: "987654321",
+        image: "/placeholder.svg",
+    },
+];
